9e38c1b06c7e59feac4034a1121d3db7272afcc3
Crone <defrag163@mail.ru>
2016-01-19 13:15:44 +0300

    School-Softdesign

---
+++ app/src/main/java/com/softdesign/school/data/storage/models/Test.java
@@ -0,0 +1,7 @@
+package com.softdesign.school.data.storage.models;
+
+/**
+ * Created by CRN_soft on 19.01.2016.
+ */
+public class Test {
+}
---
+++ app/src/main/java/com/softdesign/school/ui/adaptres/Test.java
@@ -0,0 +1,7 @@
+package com.softdesign.school.ui.adaptres;
+
+/**
+ * Created by CRN_soft on 19.01.2016.
+ */
+public class Test {
+}
